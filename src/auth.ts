703a71ccbd5cf78d649f419b509664f0be8bc583
Priveetee <brahim.benyoucefpro@proton.me>
2025-12-10 10:42:57 +0100

    feat(auth): add user registration page

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "@/lib/prisma";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL ?? "file:./dev.db",
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "sqlite",
-  }),
+  database: prismaAdapter(prisma, { provider: "sqlite" }),
   emailAndPassword: {
     enabled: true,
   },
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,14 +2,12 @@ import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL ?? "file:./dev.db";
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaBetterSqlite3({
   url: connectionString,
 });
 
-const prisma = new PrismaClient({
+export const prisma = new PrismaClient({
   adapter,
 });
-
-export { prisma };
